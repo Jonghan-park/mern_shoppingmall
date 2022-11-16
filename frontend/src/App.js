1e8c797c0c720a0716c67201d7989453c495b849
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-16 11:11:11 -0700

    Update images and others

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,218 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">
+              Joseph's Shopping mall
+            </a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div className="row center">
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                <img
+                  className="medium"
+                  src="./images/product-1.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>USB mini wind power handheld Fan</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star-half-alt"></i>
+                  </span>
+                </div>
+                <div className="price">$0.95</div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
