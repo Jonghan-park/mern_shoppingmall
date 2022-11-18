606c0eed28618f05ef5b7aa6c30e663523b8ddba
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-18 15:08:43 -0700

    Update product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import data from "./data";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -11,43 +12,14 @@ function App() {
             </a>
           </div>
           <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
           </div>
         </header>
         <main>
           <div className="row center">
             {data.products.map((product) => {
-              return (
-                <div className="card" key={product._id}>
-                  <a href={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt="product" />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
-              );
+              return <Product product={product} />;
             })}
           </div>
         </main>
