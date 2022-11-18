565b7e9226f9d879b11917f4d93bac35163b5f2a
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-18 16:01:31 -0700

    Create screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,38 @@
 import data from "./data";
 import Product from "./components/Product";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="index.html">
-              Joseph's Shopping mall
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <div className="row center">
-            {data.products.map((product) => {
-              return <Product product={product} />;
-            })}
-          </div>
-        </main>
-        <footer className="row center">All right reserved</footer>
+    <Router>
+      <div className="App">
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="index.html">
+                Joseph's Shopping mall
+              </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="product/:id" element={<ProductScreen />} />
+              <div className="row center">
+                {data.products.map((product) => {
+                  return <Product product={product} />;
+                })}
+              </div>
+            </Routes>
+          </main>
+          <footer className="row center">All right reserved</footer>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
