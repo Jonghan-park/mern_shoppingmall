2aa093fb5f0b0113d92affbc98a163ac5e10ac24
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-23 14:48:52 -0700

    Update LoadingBox and MessageBox

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
-import data from "../data";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => {
-          return <Product product={product} />;
-        })}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => {
+            return <Product product={product} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
