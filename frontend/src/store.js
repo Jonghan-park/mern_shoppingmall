400ab13ceec3e0a5007e81cae79a42b7f902999a
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-27 11:06:33 -0700

    Configure redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,11 @@
 import data from "./data";
+import { legacy_createStore as createStore } from "redux";
 
 const initialState = {};
 const reducer = (state, action) => {
   return { products: data.products };
 };
+
+const store = createStore(reducer, initialState);
+
+export default store;
